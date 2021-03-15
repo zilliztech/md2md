97a02aab2ab5a01ae3f055c49150728f65f3ac37
nameczz <zizhao.chen@zilliz.com>
2021-03-15 14:56:36 +0800

    change m2m.config.js to json

--- src/Consts.js
+++ src/Consts.js
@@ -1,20 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
 const path_config = `${
   (process.env && process.env.PATH_ROOT) || process.cwd()
-}${'/'}m2m.config.js`;
+}${"/"}m2m.config.json`;
 let config = {};
 if (fs.existsSync(path_config)) {
   config = require(path_config);
 }
 const name_dir_from =
-  process.env.name_dir_from || config.name_dir_from || 'doc_from';
-const name_dir_to = config.name_dir_to || 'doc_to';
-const name_dir_fragments = config.name_dir_fragments || 'fragments';
-const name_dir_images = 'images';
-const name_dir_templates = config.name_dir_templates || 'templates';
-const name_file_variables = config.name_file_variables || 'Variables.json';
-const name_file_glossary = config.name_file_glossary || 'Glossary.json';
+  process.env.name_dir_from || config.name_dir_from || "doc_from";
+const name_dir_to = config.name_dir_to || "doc_to";
+const name_dir_fragments = config.name_dir_fragments || "fragments";
+const name_dir_images = "images";
+const name_dir_templates = config.name_dir_templates || "templates";
+const name_file_variables = config.name_file_variables || "Variables.json";
+const name_file_glossary = config.name_file_glossary || "Glossary.json";
 
 const ignore_files = config.ignore_files || [];
 const ignore_directories = config.ignore_directories || [];
@@ -31,12 +30,12 @@ const dir_filtered = [
 const all_filtered = [...file_filtered, ...dir_filtered];
 
 const FileType = {
-  template: 'template',
-  fragment: 'fragment',
-  variable: 'variable',
-  normalDoc: 'normalDoc',
-  templateVar: 'templateVar', // json file who use template
-  others: 'others', // other common files
+  template: "template",
+  fragment: "fragment",
+  variable: "variable",
+  normalDoc: "normalDoc",
+  templateVar: "templateVar", // json file who use template
+  others: "others", // other common files
 };
 
 module.exports = {
